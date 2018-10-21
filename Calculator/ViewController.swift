17bc4eed02acfa0462d93b7f3c600e84480c6969
yeet <yeet@yeets-Mac.local>
2018-10-21 11:58:43 -0700

    Init

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by yeet on 10/21/18.
+//  Copyright © 2018 yeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
