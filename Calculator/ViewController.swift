24b7556423adf5dbd58e8ddd8a93d74b1212a3fa
yeet <yeet@yeets-Mac.local>
2018-10-28 12:14:42 -0700

    Add outlet & action

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        label.text = "7"
+    }
+    
+    @IBAction func tapACbutton(_ sender: Any) {
+        label.text = "0"
+    }
 }
-
