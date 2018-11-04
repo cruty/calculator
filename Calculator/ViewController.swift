5d44a7553a4cc2fbaa7ca10ff7ebc0c04ab201d1
yeet <yeet@yeets-Mac.local>
2018-11-04 13:08:55 -0800

    start using autolayout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,11 +17,42 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func tap7Button(_ sender: Any) {
-        label.text = "7"
-    }
-    
     @IBAction func tapACbutton(_ sender: Any) {
         label.text = "0"
     }
+    @IBAction func tap1Cbutton(_ sender: Any) {
+        if label.text == "0" {
+            label.text = "1"
+        } else {
+            label.text = label.text! + "1"
+            label.sizeToFit()
+        }
+    }
+    @IBAction func tap2Cbutton(_ sender: Any) {
+        label.text = "2"
+    }
+    @IBAction func tap3Cbutton(_ sender: Any) {
+        label.text = "3"
+    }
+    @IBAction func tap4Cbutton(_ sender: Any) {
+        label.text = "4"
+    }
+    @IBAction func tap5Cbutton(_ sender: Any) {
+        label.text = "5"
+    }
+    @IBAction func tap6Cbutton(_ sender: Any) {
+        label.text = "6"
+    }
+    @IBAction func tap7Button(_ sender: Any) {
+        label.text = "7"
+    }
+    @IBAction func tap8Cbutton(_ sender: Any) {
+        label.text = "8"
+    }
+    @IBAction func tap9Cbutton(_ sender: Any) {
+        label.text = "9"
+    }
+    
+        
+  
 }
